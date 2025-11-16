35214fce99164490808ddd7e8f5a32c04b4c8442
Matt Zhang <set.stun@gmail.com>
2025-11-16 03:21:26 -0800

    Improved question widget UX

--- components/question-widget/main.js
+++ components/question-widget/main.js
@@ -2,53 +2,83 @@ export function initComponent(getNewQuestion, totalQuestions, targetTime) {
     // page elements
     const question = document.getElementById("question");
     const answerBox = document.getElementById("answer");
-    const correctCountDisplay = document.getElementById("nCorrect");
-    const startButton = document.getElementById("startButton");
-    const timer = document.getElementById("timer");
-    const endMessage = document.getElementById("endMessage");
+    const countDisplay = document.getElementById("count");
+    const progressBarFill = document.getElementById("progress-bar-fill");
+    const timerBarFill = document.getElementById("timer-bar-fill");
 
     // state
+    let gameStarted = false;
+    let inputDisabled = false;
     let correctCount = 0;
     let startTime;
     let timerId;
     let currentAnswer = null;
 
-    // start button
-    startButton.addEventListener("click", startGame);
-    function startGame() {
-        startTime = Date.now();
-        timerId = setInterval(() => {
-            const elapsed = Math.floor((Date.now() - startTime) / 1000);
-            timer.textContent = elapsed;
-        }, 1000);
-        document.getElementById("timerDisplay").style.display = "block";
-        document.getElementById("questionCount").style.display = "block";
+    function resetGame() {
+        gameStarted = false;
+        inputDisabled = false;
+        correctCount = 0;
+        question.innerHTML = "Press Enter";
+        question.style.textAlign = "center";
+        answerBox.textContent = "";
         answerBox.focus();
     }
 
-    // display new question
+    function startGame() {
+        gameStarted = true;
+        inputDisabled = false;
+        correctCount = 0;
+        startTime = Date.now();
+        const tickInterval = 100;
+        timerId = setInterval(() => {
+            const elapsed = Date.now() - startTime;
+            if (elapsed > targetTime * 1000) {  // allow one tick over as a grace period
+                endGame(false);
+            }
+            timerBarFill.style.width = elapsed / (targetTime * 10) + "%";
+        }, tickInterval);
+        displayNewQuestion();
+    }
+
     function displayNewQuestion() {
         let newQuestion = getNewQuestion();
         question.innerHTML = newQuestion["question"];
+        question.style.textAlign = "right";
         currentAnswer = newQuestion["answer"];
         answerBox.value = '';
         answerBox.focus();
+    }
+
+    function flashAnswerBox(color) {
+        answerBox.animate(
+            [
+                { backgroundColor: color },
+                { backgroundColor: "var(--color-light)" }
+            ],
+            { duration: 300, fill: "forwards" }
+        );
     }
 
     // check answer
     answerBox.addEventListener(
         "keydown",
         e => {
-            if (e.key === "Enter") {
+            if (e.key === "Enter" && !inputDisabled) {
                 e.preventDefault();
-                if (currentAnswer === Number(answerBox.value)) {
+                if (!gameStarted) {
+                    startGame();
+                } else if (currentAnswer === Number(answerBox.value)) {
+                    flashAnswerBox("green");
                     correctCount++;
-                    correctCountDisplay.textContent = correctCount;
+                    progressBarFill.style.width = correctCount / totalQuestions * 100 + "%";
+                    countDisplay.textContent = correctCount;
                     if (correctCount === totalQuestions) {
-                        endGame();
+                        endGame(true);
+                    } else {
+                        displayNewQuestion();
                     }
-                    displayNewQuestion();
                 } else {
+                    flashAnswerBox("red");
                     answerBox.value = "";
                     answerBox.focus();
                 }
@@ -56,21 +86,20 @@ export function initComponent(getNewQuestion, totalQuestions, targetTime) {
         }
     )
 
-    // end game
-    function endGame() {
-        const endTime = Date.now();
-        const elapsed = endTime - startTime;
+    function endGame(win) {
+        gameStarted = false;
+        inputDisabled = true;
         clearInterval(timerId);
-        endMessage.style.display = "block";
-        if (elapsed <= targetTime) {
-            endMessage.textContent = "You Win!";
+        if (win) {
+            question.textContent = "You Did It!";
         } else {
-            endMessage.textContent = "Not Fast Enough!";
+            question.textContent = "Too Slow!";
+            setTimeout(resetGame, 3000);
         }
+        question.style.textAlign = "center";
     }
 
     // initialization
-    document.getElementById("totalQuestions").textContent = totalQuestions;
-    document.getElementById("targetTime").textContent = targetTime;
-    displayNewQuestion();
+    document.getElementById("total-questions").textContent = totalQuestions;
+    resetGame();
 }
