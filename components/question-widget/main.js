6c1d3b354477a302b4968784bf6d3fa3157a140e
Matt Zhang <set.stun@gmail.com>
2025-11-23 21:53:17 -0600

    Added sidebar

--- components/question-widget/main.js
+++ components/question-widget/main.js
@@ -1,10 +1,10 @@
 export function initComponent(getNewQuestion, totalQuestions, targetTime) {
     // page elements
-    const question = document.getElementById("question");
-    const answerBox = document.getElementById("answer");
-    const countDisplay = document.getElementById("count");
-    const progressBarFill = document.getElementById("progress-bar-fill");
-    const timerBarFill = document.getElementById("timer-bar-fill");
+    const question = document.getElementById('question');
+    const answerBox = document.getElementById('answer');
+    const countDisplay = document.getElementById('count');
+    const progressBarFill = document.getElementById('progress-bar-fill');
+    const timerBarFill = document.getElementById('timer-bar-fill');
 
     // state
     let gameStarted = false;
@@ -18,9 +18,9 @@ export function initComponent(getNewQuestion, totalQuestions, targetTime) {
         gameStarted = false;
         inputDisabled = false;
         correctCount = 0;
-        question.innerHTML = "Press Enter";
-        question.style.textAlign = "center";
-        answerBox.textContent = "";
+        question.innerHTML = 'Press Enter';
+        question.style.textAlign = 'center';
+        answerBox.textContent = '';
         answerBox.focus();
     }
 
@@ -35,16 +35,16 @@ export function initComponent(getNewQuestion, totalQuestions, targetTime) {
             if (elapsed > targetTime * 1000) {  // allow one tick over as a grace period
                 endGame(false);
             }
-            timerBarFill.style.width = elapsed / (targetTime * 10) + "%";
+            timerBarFill.style.width = elapsed / (targetTime * 10) + '%';
         }, tickInterval);
         displayNewQuestion();
     }
 
     function displayNewQuestion() {
         let newQuestion = getNewQuestion();
-        question.innerHTML = newQuestion["question"];
-        question.style.textAlign = "right";
-        currentAnswer = newQuestion["answer"];
+        question.innerHTML = newQuestion['question'];
+        question.style.textAlign = 'right';
+        currentAnswer = newQuestion['answer'];
         answerBox.value = '';
         answerBox.focus();
     }
@@ -53,24 +53,24 @@ export function initComponent(getNewQuestion, totalQuestions, targetTime) {
         answerBox.animate(
             [
                 { backgroundColor: color },
-                { backgroundColor: "var(--color-light)" }
+                { backgroundColor: 'var(--color-light)' }
             ],
-            { duration: 300, fill: "forwards" }
+            { duration: 300, fill: 'forwards' }
         );
     }
 
     // check answer
     answerBox.addEventListener(
-        "keydown",
+        'keydown',
         e => {
-            if (e.key === "Enter" && !inputDisabled) {
+            if (e.key === 'Enter' && !inputDisabled) {
                 e.preventDefault();
                 if (!gameStarted) {
                     startGame();
                 } else if (currentAnswer === Number(answerBox.value)) {
-                    flashAnswerBox("green");
+                    flashAnswerBox('green');
                     correctCount++;
-                    progressBarFill.style.width = correctCount / totalQuestions * 100 + "%";
+                    progressBarFill.style.width = correctCount / totalQuestions * 100 + '%';
                     countDisplay.textContent = correctCount;
                     if (correctCount === totalQuestions) {
                         endGame(true);
@@ -78,8 +78,8 @@ export function initComponent(getNewQuestion, totalQuestions, targetTime) {
                         displayNewQuestion();
                     }
                 } else {
-                    flashAnswerBox("red");
-                    answerBox.value = "";
+                    flashAnswerBox('red');
+                    answerBox.value = '';
                     answerBox.focus();
                 }
             }
@@ -91,15 +91,15 @@ export function initComponent(getNewQuestion, totalQuestions, targetTime) {
         inputDisabled = true;
         clearInterval(timerId);
         if (win) {
-            question.textContent = "You Did It!";
+            question.textContent = 'You Did It!';
         } else {
-            question.textContent = "Too Slow!";
+            question.textContent = 'Too Slow!';
             setTimeout(resetGame, 3000);
         }
-        question.style.textAlign = "center";
+        question.style.textAlign = 'center';
     }
 
     // initialization
-    document.getElementById("total-questions").textContent = totalQuestions;
+    document.getElementById('total-questions').textContent = totalQuestions;
     resetGame();
 }
--- tools/inject-component.js
+++ tools/inject-component.js
@@ -1,6 +1,6 @@
 export async function injectComponent(elementId, componentFilePath, ...params) {
     const element = document.getElementById(elementId);
-    if (!element) throw new Error(`No element with id "${elementId}" found`);
+    if (!element) throw new Error(`No element with id '${elementId}' found`);
 
     const component = await fetch(`${componentFilePath}/main.html`);
     if (!component.ok) throw new Error(`Fetch failed: ${component.status} ${component.statusText}`);
